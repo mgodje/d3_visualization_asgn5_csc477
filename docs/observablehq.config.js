3e6c58321023b56dc20d1086797992e71aee93b4
mgodje <114205884+mgodje@users.noreply.github.com>
2026-05-28 15:44:36 -0700

    yml deployment

--- docs/observablehq.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  title: "US Life Expectancy Explorer",
-  root: "docs",
-  base: "/d3_visualization_asgn5_csc477/"
-};
--- observablehq.config.js
+++ observablehq.config.js
@@ -1,5 +1,4 @@
 export default {
   title: "US Life Expectancy Explorer",
-  root: "docs",
-  base: "/d3_visualization_asgn5_csc477/"
+  root: "src",
 };
